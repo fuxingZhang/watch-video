1f1f60f38e28c5768b7a1f5d0bb6266b6f3ea92b
star <502545703@qq.com>
2019-12-03 21:19:43 +0800

    reg match video name

--- app.js
+++ app.js
@@ -7,37 +7,41 @@ const hostname = '0.0.0.0';
 const port = 3000;
 const stat = fs.statSync('./public/zfx.mp4');
 const size = stat.size;
+const reg = /^\/([^\/]+?)$/i;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  switch (req.url) {
-    case '/':
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(fs.readFileSync('./public/index.html'))
-      break;
-    case '/zfx.mp4':
-      const range = parseRange(req.headers.range, size);
-      const { start, end } = range;
-      res.statusCode = 206
-      res.setHeader('accept-ranges', 'bytes');
-      res.setHeader('content-type', 'video/mp4');
-      res.setHeader('cache-control', 'public, max-age=0');
-      res.setHeader('last-modified', stat.mtime.toUTCString());
-      res.setHeader('ETag', etag(stat))
-      res.setHeader('Content-Range', contentRange(size, range));
-      res.setHeader('Content-Length', end - start + 1)
-      const stream = fs.createReadStream('./public/zfx.mp4', {
-        start,
-        end
-      });
-      stream.on('error', err => {
-        stream.destroy();
-      });
-      stream.pipe(res);
-      break;
-    default:
-      res.end('hi');
+  const url = req.url;
+  console.log({ url });
+  const arr = reg.exec(url);
+
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(fs.readFileSync('./public/index.html'));
+    return
+  } else if(arr !== null) {
+    const videoName = arr[1];
+    const range = parseRange(req.headers.range, size);
+    const { start, end } = range;
+    res.statusCode = 206;
+    res.setHeader('accept-ranges', 'bytes');
+    res.setHeader('content-type', 'video/mp4');
+    res.setHeader('cache-control', 'public, max-age=0');
+    res.setHeader('last-modified', stat.mtime.toUTCString());
+    res.setHeader('ETag', etag(stat));
+    res.setHeader('Content-Range', contentRange(size, range));
+    res.setHeader('Content-Length', end - start + 1);
+    const stream = fs.createReadStream(`./public/${videoName}`, {
+      start,
+      end
+    });
+    stream.on('error', err => {
+      console.log('stream error:', err);
+      stream.destroy();
+    });
+    stream.pipe(res);
+  } else {
+    res.end('hi, i am zfx!');
   }
 });
 
